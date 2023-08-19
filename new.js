22da0ed76556b64398e9fc5b5157801006a380bc
Princewilln <prinzy101@gmail.com>
2023-08-19 17:42:20 -0500

    addiding recent changes

--- new.js
+++ new.js
@@ -328,49 +328,51 @@ to get and set content? */
 
 
 
-// test.forEach(function);
-// const bills = [50.23, 19.12, 34.01, 100.11, 12.15, 9.90, 29.11, 12.99, 10.00, 99.22, 102.20, 100.10, 6.77, 2.22];
+test.forEach(function);
+const bills = [50.23, 19.12, 34.01, 100.11, 12.15, 9.90, 29.11, 12.99, 10.00, 99.22, 102.20, 100.10, 6.77, 2.22];
 
-// const totals = bills.map(function (item, i) {
-//     // if (item % 3 === 0) {
-//     //   console.log("Element at index " + index + " is divisible by 3");
-//     //   test[index] = test[index] + 100;
-//     // } else {
-//         item *= 0.15
-//         item = bills[i] + item
-//       return (Number(item.toFixed(2)));
-//     // } 
-//   });
-//   console.log(totals)
-var grid = [
-    [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
-    [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
-    [67, 56, 45, 553, 44, 55, 5, 428, 452, 3],
-    [12, 31, 55, 445, 79, 44, 674, 224, 4, 21],
-    [4, 2, 3, 52, 13, 51, 44, 1, 67, 5],
-    [5, 65, 4, 5, 5, 6, 5, 43, 23, 4424],
-    [74, 532, 6, 7, 35, 17, 89, 43, 43, 66],
-    [53, 6, 89, 10, 23, 52, 111, 44, 109, 80],
-    [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
-    [76, 7, 9, 6, 3, 73, 77, 100, 56, 100]
-];
-// const newGrid = grid.map(function(item,i,d) {
-  for (let r = 0; r < grid.length; r++) {
-    for (let c = 0; c < grid[r].length; c++) {
-        if (grid[r][c] % 2 === 0) {
-            grid[r][c] = "even" ;
-        }
-           else {
-            grid[r][c] = "odd" ;
-        }
-      }
+const totals = bills.map(function (item, i) {
+    // if (item % 3 === 0) {
+    //   console.log("Element at index " + index + " is divisible by 3");
+    //   test[index] = test[index] + 100;
+    // } else {
+        item *= 0.15
+        item = bills[i] + item
+      return (Number(item.toFixed(2)));
+    // } 
+  });
+  console.log(totals)
+
+
+// var grid = [
+//     [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
+//     [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
+//     [67, 56, 45, 553, 44, 55, 5, 428, 452, 3],
+//     [12, 31, 55, 445, 79, 44, 674, 224, 4, 21],
+//     [4, 2, 3, 52, 13, 51, 44, 1, 67, 5],
+//     [5, 65, 4, 5, 5, 6, 5, 43, 23, 4424],
+//     [74, 532, 6, 7, 35, 17, 89, 43, 43, 66],
+//     [53, 6, 89, 10, 23, 52, 111, 44, 109, 80],
+//     [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
+//     [76, 7, 9, 6, 3, 73, 77, 100, 56, 100]
+// ];
+// // const newGrid = grid.map(function(item,i,d) {
+//   for (let r = 0; r < grid.length; r++) {
+//     for (let c = 0; c < grid[r].length; c++) {
+//         if (grid[r][c] % 2 === 0) {
+//             grid[r][c] = "even" ;
+//         }
+//            else {
+//             grid[r][c] = "odd" ;
+//         }
+//       }
    
-    //  return d;
+//     //  return d;
       
-  }
+//   }
 
-//   });
-  console.log(grid);
+// //   });
+//   console.log(grid);
 
 
 
